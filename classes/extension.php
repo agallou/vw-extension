f51bb055376a0da9b6bf27f9eff45c4bb82b3451
Adrien Gallou <adriengallou@gmail.com>
2015-10-08 19:32:08 +0200

    fix code conventions

--- classes/extension.php
+++ classes/extension.php
@@ -9,6 +9,9 @@ use mageekguy\atoum\test;
 
 class extension implements atoum\extension
 {
+    /**
+     * @var array
+     */
     private $examinators = array(
         'CI',
         'CONTINUOUS_INTEGRATION',
@@ -25,52 +28,34 @@ class extension implements atoum\extension
         'BUILDKITE',
     );
 
-	/**
-	 * @param runner $runner
-	 * @return $this
-	 */
-	public function setRunner(runner $runner)
-	{
-		return $this;
-	}
+    /**
+     * @param runner $runner
+     * @return $this
+     */
+    public function setRunner(runner $runner)
+    {
+        return $this;
+    }
 
-	/**
-	 * @param test $test
-	 * @return $this
-	 */
-	public function setTest(test $test)
-	{
-		return $this;
-	}
+    /**
+     * @param test $test
+     * @return $this
+     */
+    public function setTest(test $test)
+    {
+        return $this;
+    }
 
-	/**
-	 * @param string     $event
-	 * @param observable $observable
-	 */
-	public function handleEvent($event, observable $observable) {
+    /**
+     * @param string $event
+     * @param observable $observable
+     */
+    public function handleEvent($event, observable $observable)
+    {
 
         if ($this->underScrutiny() && $event == atoum\test::beforeTearDown && $observable instanceof \mageekguy\atoum\test) {
-           $observable->setScore($this->getCleanedScore($observable->getScore()));
+            $observable->setScore($this->getCleanedScore($observable->getScore()));
         }
-	}
-
-    protected function getCleanedScore(\mageekguy\atoum\test\score $score)
-    {
-        $newScore = new \mageekguy\atoum\test\score();
-        $newScore->getCoverage($score->getCoverage());
-        foreach ($score->getDurations() as $duration) {
-            $newScore->addDuration($duration['path'], $duration['class'], $duration['method'], $duration['value']);
-        }
-        foreach ($score->getMemoryUsages() as $memoryUsage) {
-            $newScore->addMemoryUsage(null, $memoryUsage['class'], $memoryUsage['method'], $memoryUsage['value']);
-        }
-
-        $assertions = $score->getPassNumber() + $score->getFailNumber();
-        for ($i=0; $i<=$assertions; $i++) {
-            $newScore->addPass();
-        }
-
-        return $newScore;
     }
 
     /**
@@ -86,6 +71,25 @@ class extension implements atoum\extension
             }
         }
         return false;
+    }
+
+    protected function getCleanedScore(\mageekguy\atoum\test\score $score)
+    {
+        $newScore = new \mageekguy\atoum\test\score();
+        $newScore->getCoverage($score->getCoverage());
+        foreach ($score->getDurations() as $duration) {
+            $newScore->addDuration($duration['path'], $duration['class'], $duration['method'], $duration['value']);
+        }
+        foreach ($score->getMemoryUsages() as $memoryUsage) {
+            $newScore->addMemoryUsage(null, $memoryUsage['class'], $memoryUsage['method'], $memoryUsage['value']);
+        }
+
+        $assertions = $score->getPassNumber() + $score->getFailNumber();
+        for ($i = 0; $i <= $assertions; $i++) {
+            $newScore->addPass();
+        }
+
+        return $newScore;
     }
 
 
